3c527171c4925aec1b6f982dd7f23fd3179281fb
DIEGO CAMILO MARTINS VIEIRA <dcvieira@tjsp.jus.br>
2025-01-11 16:38:22 -0300

    using redux toolkit

--- src/index.js
+++ src/index.js
@@ -6,11 +6,6 @@ import App from "./App";
 
 import store from "./store";
 
-store.dispatch({
-  type: "account/deposit",
-  payload: 1000,
-});
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
